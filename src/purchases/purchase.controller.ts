9e4879147f572764ae1747cb8800575ca7b4dfc4
panabordee <68010697@kmitl.ac.th>
2026-03-25 12:03:44 +0700

    fk name diff

--- src/purchases/purchase.controller.ts
+++ src/purchases/purchase.controller.ts
@@ -1,18 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { PurchasesService } from './purchase.service';
+import { PurchaseService } from './purchase.service';
 import type { ApiResponse } from 'src/interfaces/response.interface';
 import { Purchase } from './purchase.interface';
 
 @Controller('purchases')
 export class PurchasesController {
-  constructor(private readonly productService: PurchasesService) {}
+  constructor(private readonly productService: PurchaseService) {}
 
   @Get()
   findAll(): ApiResponse<Purchase[]> {
     return {
       success: true,
       data: this.productService.findAll(),
-      message: 'Fetched products successfully',
+      message: 'Fetched purchases successfully"',
     };
   }
 }
--- src/purchases/purchase.service.ts
+++ src/purchases/purchase.service.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { Purchase } from './purchase.interface';
 
 @Injectable()
-export class PurchasesService {
+export class PurchaseService {
   findAll(): Purchase[] {
     const data = fs.readFileSync('data/purchases.json', 'utf-8');
     const product = JSON.parse(data) as Purchase[];
